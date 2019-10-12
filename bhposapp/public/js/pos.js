9c59fcc91e19b2426140751bbc36087edffa0d39
John Rech Cabatana <cabatana.johnrech.g@gmail.com>
2019-10-12 08:03:48 +0800

    MODIFIED CSS

--- bhposapp/public/js/pos.js
+++ bhposapp/public/js/pos.js
@@ -1484,7 +1484,7 @@ class POSItems {
     		  
  	 
 		const price_list_rate = format_currency(item.price_list_rate, this.currency);
-		const { item_code, item_name, item_image, actual_qty, end_of_life} = item;
+		const { item_code, item_name, item_image, actual_qty, end_of_life, size, item_type} = item;
 		const item_title = item_name || item_code;
 		const qty = this.get_item_this(item_code,this.frm.doc.pos_profile);
 		console.log(qty);
@@ -1502,10 +1502,10 @@ class POSItems {
 			<div class="pos-item-wrapper image-view-item  ${classname}" data-item-code="${escape(item_code)}"> 
 				<a class="grey list-id" data-name="${item_code}" title="${item_title}">
 					<div class="row">
-						<div class="col-xs-3">${item_title} </div>
+						<div class="col-xs-3">${item_title} ~ ${size} </div>
 						<div class="col-xs-3 text-right">${price_list_rate}</div>
 						<div class="col-xs-3 text-center qty-m"><span>${act_qty}<span></div>
-						<div class="col-xs-3 text-center ">${end_of_life}</div>
+						<div class="col-xs-3 text-center ">${end_of_life} ${item_type} </div>
 					</div>
 				</a> 
 			</div>
