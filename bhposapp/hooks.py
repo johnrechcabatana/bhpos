d85f28d66ad3b66a137263e97ac907a6a30ec4a5
johndef <cabatana.johnrech.g@gmail.com>
2019-10-23 15:45:07 +0800

    changes hid some field of Item doctype

--- bhposapp/hooks.py
+++ bhposapp/hooks.py
@@ -35,7 +35,6 @@ doctype_js = {
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
-
 fixtures = [
     {
         "doctype": "Custom Field",
